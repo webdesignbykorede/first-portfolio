36e6ad63d1f55eb298ea17e228ab0355610d61e9
webdesignbykorede <oyewokorede999@gmail.com>
2026-01-31 08:37:38 -1200

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-window.addEventListener('DOMContentLoaded', () => {
-    alert('Welcome to my little page');
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+        const target=document.querySelector(this.getAttribute('href'));
+        if (target) {
+            target.scrollIntoView({ behavior: 'smooth' });
+        }
+    });
 });
+
